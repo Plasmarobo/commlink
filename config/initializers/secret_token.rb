34fc1aa86cdbd69335cc1114a72dac2078d81392
plasmarobo <plasmarobo@gmail.com>
2013-05-01 14:56:43 -0400

    Migration to Postgresql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Commlink::Application.config.secret_token = '642a2e285b70ce6c6ad416344fe5b647ce5f504ce9c24204e3c4efb8e5df0c3be4d784ae88fef74b23ff32f01b1bde54278293e8102fa0c39fdfac6e93582801'
+Commlink::Application.config.secret_token = '9a7f0395fbfe7996caaf286a897a9e6f1e979c7aab5a2f4c7a609ed43cbb455b7af2f9a28be2f60d8328f5fe63c501103f0bcf40b4fee81ca4f8e45b3e45a046'
