e5bfb65877d05e9cd3db51b99cd22f3c9d112717
austenh <austenh@railsvm.(none)>
2013-05-01 13:52:34 -0400

    Default Rails App Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commlink::Application.config.secret_token = '642a2e285b70ce6c6ad416344fe5b647ce5f504ce9c24204e3c4efb8e5df0c3be4d784ae88fef74b23ff32f01b1bde54278293e8102fa0c39fdfac6e93582801'
